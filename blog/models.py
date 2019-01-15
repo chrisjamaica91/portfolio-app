0e70bffb72ff6b243eca079345cfb397fad01ed2
chrisjamaica <chriscargill2761@gmail.com>
2019-01-14 19:25:23 -0500

    added in an html template page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create a Blog model
+#title
+#publication date
+#text in post
+#image
+class Blog(models.Model):
+    title = models.CharField(max_length = 255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
+
+# Add the blog app to settings
+#create a migration
+#Migrate
+#Add to the admin
